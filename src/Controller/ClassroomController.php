cadf8f658ef3fe1c7b9581f2ed5860c51bba31b8
Fourat Abdellatif <fourat-abdellatif99@hotmail.fr>
2022-02-28 14:30:50 +0100

    add updateClassroom function

--- src/Controller/ClassroomController.php
+++ src/Controller/ClassroomController.php
@@ -55,9 +55,29 @@ class ClassroomController extends AbstractController
         $form = $this->createForm(ClassroomType::class, $c);
         /*$c->setName($name);*/
         $form->handleRequest($req);
-        if($form->isSubmitted()){
+        if ($form->isSubmitted()) {
             $em = $this->getDoctrine()->getManager();
             $em->persist($c);
+            $em->flush();
+            return $this->redirectToRoute("list_classroom");
+        }
+        return $this->render('classroom/add.html.twig', [
+            'myForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/classroom/list/updateClass/{id}", name="update_class")
+     */
+    public function updateClassroom($id, Request $req): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Classroom::class);
+        $c = $repo->find($id);
+        $form = $this->createForm(ClassroomType::class, $c);
+        /*$c->setName($name);*/
+        $form->handleRequest($req);
+        if ($form->isSubmitted()) {
+            $em = $this->getDoctrine()->getManager();
             $em->flush();
             return $this->redirectToRoute("list_classroom");
         }
